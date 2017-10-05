344e1223b07e89656cfe5d0b08cb131e641302b5
Dominik Reichl <dominik.reichl@t-online.de>
2017-10-05 11:42:08 +0200

    Release 1.34: KeePass-1.34-Src.zip
    Source: https://downloads.sourceforge.net/project/keepass/KeePass%201.x/1.34/KeePass-1.34-Src.zip

--- WinGUI/NewGUI/NewGUICommon.cpp
+++ WinGUI/NewGUI/NewGUICommon.cpp
@@ -891,7 +891,7 @@ BOOL NewGUI_GetNonClientMetrics(NONCLIENTMETRICS* p)
 
 	// See the documentation of the NONCLIENTMETRICS structure:
 	// https://msdn.microsoft.com/en-us/library/windows/desktop/ff729175.aspx
-	// http://www.codeproject.com/Messages/3989684/Compile-with-dev-studio-running-on-Windows-XP.aspx
+	// https://www.codeproject.com/Messages/3989684/Compile-with-dev-studio-running-on-Windows-XP.aspx
 #if (WINVER == 0x0600)
 	if(AU_IsAtLeastWinVistaSystem() == FALSE)
 	{
